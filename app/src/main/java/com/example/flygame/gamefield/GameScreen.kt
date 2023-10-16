4545529c9906b205577cb96af3a0445533f0b4eb
DmitriyZakharich <zakharichd@yandex.ru>
2023-10-16 21:25:43 +0300

    Adjusted the size of the arrows

--- app/src/main/java/com/example/flygame/gamefield/GameScreen.kt
+++ app/src/main/java/com/example/flygame/gamefield/GameScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
@@ -49,15 +50,14 @@ fun Table(gameViewModel: GameViewModel) {
     val gameStatus by gameViewModel.stateGameStatus.collectAsState()
     val arrowCommand = gameViewModel.stateCommandArrow.collectAsState()
 
+    ArrowCommand(arrowCommand)
+
     if (gameStatus == GameStatus.GIVE_COMMANDS && settings.isHideField) return   //Скрывать поле во время команд
 
     if (!settings.isVolume)
         FlatField(settings, gameViewModel)
     else
         VolumetricField(settings, gameViewModel)
-
-    ArrowCommand(arrowCommand)
-
 }
 
 @Composable
@@ -69,13 +69,13 @@ fun FlatField(
     val configuration = LocalConfiguration.current
     val screenWidth = configuration.screenWidthDp.dp
 
-    val paddingTop = if (settings.isVolume)  0.dp else 50.dp
+    val paddingTop = if (settings.isVolume)  0.dp else 55.dp
 
     Column(
         modifier = Modifier
+            .padding(start = 5.dp, end = 5.dp, top = paddingTop, bottom = 5.dp)
             .fillMaxWidth()
-            .height(screenWidth)
-            .padding(start = 5.dp, end = 5.dp, top = paddingTop),
+            .height(screenWidth),
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
         for (rowCount in 0 until settings.tableSize) {
@@ -123,11 +123,8 @@ fun MyCell(
             .background(backgroundColor)
             .clip(RoundedCornerShape(5.dp))
             .clickable {
-                if (gameStatus != GameStatus.STOP) {
-                    backgroundColor = if (id == coordinatesFly) {
-                        Color.Green
-                    } else
-                        Color.Red
+                if (gameStatus == GameStatus.WAITING_RESPONSE) {
+                    backgroundColor = if (id == coordinatesFly) Color.Green else Color.Red
                     gameViewModel.endGame()
                 }
             }
@@ -161,12 +158,9 @@ fun ArrowCommand(arrowCommand: State<DirectionMove>) {
     val configuration = LocalConfiguration.current
     val screenWidth = configuration.screenWidthDp.dp
 
+    val stateImage = remember { mutableIntStateOf(R.drawable.arrow_up) }
 
-
-    val stateRotate = remember {
-        mutableIntStateOf(R.drawable.arrow_up)
-    }
-    stateRotate.intValue = when (arrowCommand.value) {
+    stateImage.intValue = when (arrowCommand.value) {
         DirectionMove.UP -> R.drawable.up_red_3
         DirectionMove.DOWN -> R.drawable.down_red_3
         DirectionMove.LEFT -> R.drawable.left_red_3
@@ -177,15 +171,14 @@ fun ArrowCommand(arrowCommand: State<DirectionMove>) {
     }
 
     Image(
-        painter = painterResource(id = stateRotate.intValue),
+        painter = painterResource(id = stateImage.intValue),
         contentDescription = "icon",
         modifier = Modifier
+            .padding(start = 15.dp, end = 15.dp, top = 65.dp)
             .fillMaxWidth()
-            .height(screenWidth)
-            .padding(start = 5.dp, end = 5.dp, top = 50.dp)
-            .clip(CircleShape)
+            .height(screenWidth - 20.dp)
             .zIndex(1f)
-            .alpha(0.5f)
+            .alpha(0.55f)
     )
 }
 
